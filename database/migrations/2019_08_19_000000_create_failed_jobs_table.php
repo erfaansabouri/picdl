9ededaf9574e49f2d2fd2f229fcf6bbbbd286a5e
Erfan Sabouri <erfaansabouri@gmail.com>
2022-06-19 23:54:29 +0430

    Fix

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -15,7 +15,7 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->text('uuid')->unique();
+            $table->text('uuid')->nullable();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
